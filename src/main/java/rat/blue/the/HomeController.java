f652ebe3bf2bdc723a837fbe118bc55721c9ae4f
GD7 <GD7@DESKTOP-58VHOFH>
2019-09-10 18:22:33 +0900

    파일 경로 넣기

--- src/main/java/rat/blue/the/HomeController.java
+++ src/main/java/rat/blue/the/HomeController.java
@@ -1,16 +1,54 @@
 package rat.blue.the;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value = "/")
+//	@RequestMapping(value = "/login")
+//	public String home() {
+//		return "login";
+//	}
+//	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return "login";
+		return "home";
 	}
 	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String file(MultipartFile file) {
+		System.out.println(file.getOriginalFilename());
+		
+		try {
+			byte[] data =  file.getBytes(); //전송했던 파일 안의 내용물. 데이터가 꺼내짐
+			String path = "D:\\IDE\\workspace2\\20190905\\src\\main\\webapp\\resources\\upload";
+			File f = new File(path); //경로 넣기 <ㄱ
+			
+			
+//			InputStream is = file.getInputStream();
+//			InputStreamReader isr = new InputStreamReader(is);
+//			BufferedReader br = new BufferedReader(isr);
+//			StringBuilder sb = new StringBuilder();
+//			String txt = "";
+//			String result = "";
+//			while((txt = br.readLine()) !=null) result += txt + "\n";
+//			System.out.println(result);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "home";
+	}
 	
 	
 }
